fb893a941d5278c42b71c9d097731b3a8a5dd8ed
Charawey-X <charawey@gmail.com>
2022-05-17 16:02:15 +0300

    user instance test

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,5 +12,16 @@ class UserTest {
 
     @AfterEach
     void tearDown() {
+    }
+
+    //helper method
+    private User setupUser() {
+        return new User("Jane Doe","Nairobi","Java",true);
+    }
+
+    @Test
+    public void Animal_instantiatesCorrectly() {
+        User user = setupUser();
+        assertTrue(user instanceof User);
     }
 }
