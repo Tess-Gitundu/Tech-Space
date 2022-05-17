c06166951acc6bf03c64b55a8186143beca54b20
Tess <njerigitundu003@gmail.com>
2022-05-17 12:38:12 +0300

    Create and implement interface

--- src/main/java/Location.java
+++ src/main/java/Location.java
@@ -1,2 +1,26 @@
-public class Location {
+public class Location implements Tech{
+    @Override
+    public void add() {
+
+    }
+
+    @Override
+    public void findById(int id) {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void clearAll() {
+
+    }
 }
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,2 +1,60 @@
-public class User {
+public class User implements Tech{
+
+    private String userName;
+    private String userLocation;
+    private int id;
+
+    public User(String userName, String userLocation) {
+        this.userName = userName;
+        this.userLocation = userLocation;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserLocation() {
+        return userLocation;
+    }
+
+    public void setUserLocation(String userLocation) {
+        this.userLocation = userLocation;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public void add() {
+
+    }
+
+    @Override
+    public void findById(int id) {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void clearAll() {
+
+    }
 }
