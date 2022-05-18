c170f1496147065bff5a9a904baa09814bc0ee25
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-05-18 14:32:55 +0300

    Add logic for adding user to database

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,14 +1,23 @@
 import static spark.Spark.*;
 
+import DAO.LocationDao;
+import DAO.UserDao;
 import Database.DB;
+import Models.User;
+import org.sql2o.Connection;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.text.Collator;
 import java.util.HashMap;
 import java.util.Map;
 
 
 public class App {
+    private static Connection conn = DB.sql2o.open();
+    private static UserDao userDao = new UserDao(DB.sql2o);
+    private static LocationDao locationDao = new LocationDao(DB.sql2o);
+
     public static void main(String[] args) {
         DB.createTables(DB.sql2o.open());
         staticFileLocation("public");
@@ -29,6 +38,21 @@ public class App {
         }, new HandlebarsTemplateEngine());
 
         post("/add", (request, response) -> {
+            String username = request.queryParams("username");
+            String location = request.queryParams("location");
+            String language = request.queryParams("language");
+            String availability = request.queryParams("available");
+            Boolean available = false;
+            Collator myCollator = Collator.getInstance();
+            try{
+                if (myCollator.compare(availability, "true") == 0){
+                    available = true;
+                }
+            } catch (Exception e){
+                available = false;
+            }
+            User user = new User(username, location, language, available);
+            userDao.save(user);
             response.redirect("spaces");
             return null;
         });
