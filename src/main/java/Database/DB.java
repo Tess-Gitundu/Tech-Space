77397516fb6370bade7bc2608ad3c54e70b4b9e8
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-05-19 09:51:32 +0300

    add heroku db connection

--- src/main/java/Database/DB.java
+++ src/main/java/Database/DB.java
@@ -6,11 +6,14 @@ import org.sql2o.Sql2oException;
 
 public class DB {
 //    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/tech_spaces", "tess", "Njeri@Postgres");
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/samian", "samian", "root");
+//    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/samian", "samian", "root");
+    // Development
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-3-231-82-226.compute-1.amazonaws.com:5432/debgbt3piakrs3", "vtanfeaouixpjq", "da6a441059ed1cc4bd2b458733e476ed1c2a5b2143bbf6774e237a5037e53df9");
+
 
     public static void createTables(Connection conn) {
         try{
-            conn.createQuery("CREATE TABLE IF NOT EXISTS users2 (\n" +
+            conn.createQuery("CREATE TABLE IF NOT EXISTS users (\n" +
                     " id SERIAL PRIMARY KEY,\n" +
                     " userName VARCHAR,\n" +
                     " userLocation INT,\n" +
